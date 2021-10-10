9d83e02030621a82626484c8e336f1f3043baed6
Abanoub Samir <abanoubsamirshenouda@gmail.com>
2021-10-10 18:27:34 +0200

    add aboutme section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutmeComponent
   ],
   imports: [
     BrowserModule
